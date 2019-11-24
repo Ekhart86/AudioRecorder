2defa519847452d3d59126a20529ec59dc980979
Ekhart86 <valderama1@yandex.ru>
2019-11-24 16:33:49 +0300

    refactoring play methods

--- app/src/main/java/ru/ekhart86/audiorecorder/play/PlayActivity.kt
+++ app/src/main/java/ru/ekhart86/audiorecorder/play/PlayActivity.kt
@@ -1,11 +1,14 @@
 package ru.ekhart86.audiorecorder.play
 
 import android.content.Intent
+import android.graphics.Color
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.SystemClock
 import android.util.Base64
 import android.util.Log
 import android.view.View
+import android.widget.Chronometer
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
@@ -22,12 +25,13 @@ private const val PLAY_TAG = "AudioPlay"
 class PlayActivity : AppCompatActivity() {
 
     private lateinit var mDate: TextView
-    private lateinit var mPlay: ImageButton
+    private lateinit var playBtn: ImageButton
     private var mediaPlayer: MediaPlayer? = null
     private var currentId: Int? = null
     private var pathWrite = ""
     private lateinit var dbHelper: DBHelper
-
+    private lateinit var chronometer: Chronometer
+    private var timeWhenStopped: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +43,8 @@ class PlayActivity : AppCompatActivity() {
         mDate = findViewById(R.id.record_date_detail_id)
         val dateString = intent.getStringExtra("date")
         mDate.text = dateString
-        mPlay = findViewById(R.id.play_button_id)
+        playBtn = findViewById(R.id.play_button_id)
+        chronometer = findViewById(R.id.view_timer_play)
         //Путь куда запишется файл из базы
         pathWrite = "${externalCacheDir!!.absolutePath}/audioPlay.mp4"
         File(pathWrite).writeBytes(
@@ -53,21 +58,34 @@ class PlayActivity : AppCompatActivity() {
 
     }
 
-
     fun clickPlayButton(v: View) {
         mediaPlayer = MediaPlayer()
         try {
             mediaPlayer!!.setDataSource(pathWrite)
             mediaPlayer!!.prepare()
             mediaPlayer!!.start()
+            playBtn.setColorFilter(Color.RED)
+            chronometer.base = SystemClock.elapsedRealtime()
+            timeWhenStopped = 0
+            chronometer.start()
+            mediaPlayer!!.setOnCompletionListener {
+                playBtn.isEnabled = true
+                playBtn.clearColorFilter()
+                chronometer.stop()
+            }
         } catch (e: IOException) {
             Log.e(PLAY_TAG, "Ошибка воспроизведения аудиозаписи.")
         }
     }
 
-    //Декодируем строковое значение из base64 в массив байтов, для дальнейшей записи в кэш и воспроизведения
-    private fun decodeBase64(value: String?): ByteArray {
-        return Base64.decode(value, Base64.DEFAULT)
+    fun clickStopPlayButton(view: View) {
+
+        if (mediaPlayer != null && mediaPlayer!!.isPlaying) {
+            chronometer.stop()
+            mediaPlayer!!.stop()
+            playBtn.clearColorFilter()
+            mediaPlayer = null
+        }
     }
 
 
@@ -85,6 +103,19 @@ class PlayActivity : AppCompatActivity() {
         finish()
     }
 
-    fun clickStopPlayButton(view: View) {}
 
+    //Декодируем строковое значение из base64 в массив байтов, для дальнейшей записи в кэш и воспроизведения
+    private fun decodeBase64(value: String?): ByteArray {
+        return Base64.decode(value, Base64.DEFAULT)
+    }
+
+    //Остановить и уничтожить плеер при выходе с активити, если воспроизведение не была завершено
+    override fun onDestroy() {
+        if (mediaPlayer != null) {
+            mediaPlayer!!.stop()
+            mediaPlayer = null
+        }
+        finish()
+        super.onDestroy()
+    }
 }
